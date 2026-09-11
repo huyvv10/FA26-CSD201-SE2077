677f8c1930d60c0bc2ec307836bfad566b7887d2
VuVanHuy <huyvv10@fpt.edu.vn>
2026-09-11 08:16:34 +0700

    commit

--- FA26_SinglyLinkedList/src/Main.java
+++ FA26_SinglyLinkedList/src/Main.java
@@ -7,9 +7,10 @@ public class Main {
         System.out.println("\n==============");
         System.out.println("1. Add first");
         System.out.println("2. Add last");
-        System.out.println("3. Count nodes");
-        System.out.println("4. Add at pos");
-        System.out.println("5. Get node at pos");
+        System.out.println("3. Display list");
+        System.out.println("4. Count nodes");
+        System.out.println("5. Add at pos");
+        System.out.println("6. Get node at pos");
         System.out.println("0. Quit");
         System.out.print("Your selection: ");
     }
@@ -42,17 +43,19 @@ public class Main {
                     myList.addLast(x);
                     break;
                 case 3:
+                    myList.display(); break;
+                case 4:
                     System.out.println("Number of nodes: "
                             + myList.countNodes());
                     break;
-                case 4:
+                case 5:
                     System.out.print("Input x = ");
                     x = sn.nextInt();
                     System.out.print("Input pos = ");
                     pos = sn.nextInt();
                     myList.insertAtPos(x, pos);
                     myList.display();
-                case 5:
+                case 6:
                     System.out.print("Input pos to get value: ");
                     pos = sn.nextInt();
                     Node xNode = myList.getNodeAtPos(pos);
